20e2a673a497cab466fee709cc98a71cab84d5bc
chaosGuppy <lawphi56@gmail.com>
2023-06-24 15:26:42 +0100

    small fix

--- bullshit_bot_bot_bot/run.py
+++ bullshit_bot_bot_bot/run.py
@@ -17,6 +17,7 @@ from bullshit_bot_bot_bot.handlers.missing_considerations import (
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -38,6 +39,10 @@ async def missing_considerations(update: Update, context: ContextTypes.DEFAULT_T
     )
 
 
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.error("Exception while handling an update:", exc_info=context.error)
+
+
 async def record_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if "updates" not in context.chat_data:
         context.chat_data["updates"] = []
@@ -49,10 +54,14 @@ if __name__ == "__main__":
 
     start_handler = CommandHandler("start", start)
     summarize_handler = CommandHandler("summarize", summarize)
-    summarize_handler = CommandHandler("missing_considerations", missing_considerations)
+    missing_considerations_handler = CommandHandler(
+        "missing_considerations", missing_considerations
+    )
 
     application.add_handler(summarize_handler)
     application.add_handler(start_handler)
+    application.add_handler(missing_considerations_handler)
     application.add_handler(MessageHandler(filters.ALL, record_messages))
+    application.add_error_handler(error_handler)
 
     application.run_polling()
