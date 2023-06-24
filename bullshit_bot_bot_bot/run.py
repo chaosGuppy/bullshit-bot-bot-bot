d533633b28a9005b8e565aa29d66de81a58c1ec1
chaosGuppy <lawphi56@gmail.com>
2023-06-24 13:38:40 +0100

    record chat history

--- bullshit_bot_bot_bot/run.py
+++ bullshit_bot_bot_bot/run.py
@@ -1,7 +1,13 @@
 import os
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -10,14 +16,22 @@ logging.basicConfig(
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+        chat_id=update.effective_chat.id, text=str(context.chat_data)
     )
+
+
+async def record_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if "updates" not in context.chat_data:
+        context.chat_data["updates"] = []
+    context.chat_data["updates"].append(update.to_dict())
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(os.environ["TELEGRAM_TOKEN"]).build()
 
     start_handler = CommandHandler("start", start)
+
     application.add_handler(start_handler)
+    application.add_handler(MessageHandler(filters.ALL, record_messages))
 
     application.run_polling()
